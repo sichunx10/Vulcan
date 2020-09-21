e9019365a2f9434d126d43d3b274d4f89a646d31
sichunx10 <33789770+sichunx10@users.noreply.github.com>
2020-09-21 10:45:27 -0400

    Update StepperMotorTest.ino
    Added comments

--- Preliminary Testing/StepperMotorTest/StepperMotorTest.ino
+++ Preliminary Testing/StepperMotorTest/StepperMotorTest.ino
@@ -5,31 +5,33 @@ const int ms1Pin = 8;
 const int ms2Pin = 9;
 const int ms3Pin = 10;
 
-const int stepperDelay = 800;
+//for Full step, use 800 delay
+//for Half step, use 400 delay, and so on
+const int stepperDelay = 1000;
  
 void setup() {
-  // Sets the two pins as Outputs
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
   pinMode(ms1Pin,OUTPUT);
+  pinMode(ms2Pin,OUTPUT);
+  pinMode(ms3Pin,OUTPUT);
 }
 void loop() {
   digitalWrite(ms1Pin,LOW);
   digitalWrite(ms2Pin,LOW);
   digitalWrite(ms3Pin,LOW);
 
-  digitalWrite(dirPin,HIGH);
-  for(int x = 0; x < 6400; x++) {
+  digitalWrite(dirPin,HIGH); //clockwise rotation
+  for(int x = 0; x < 400; x++) {
     digitalWrite(stepPin,HIGH); 
     delayMicroseconds(stepperDelay); 
     digitalWrite(stepPin,LOW); 
     delayMicroseconds(stepperDelay); 
   }
-  delay(200); // One second delay
+  delay(200); // 20 ms delay
   
-  digitalWrite(dirPin,LOW); //Changes the rotations direction
-  // Makes 400 pulses for making two full cycle rotation
-  for(int x = 0; x < 6400; x++) {
+  digitalWrite(dirPin,LOW); //counterclockwise rotation
+  for(int x = 0; x < 400; x++) {
     digitalWrite(stepPin,HIGH);
     delayMicroseconds(stepperDelay);
     digitalWrite(stepPin,LOW);
